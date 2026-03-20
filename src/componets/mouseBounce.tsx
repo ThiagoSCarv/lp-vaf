db7adda0837da6b70d2a4dd143ebaede14dfd12c
Thiago <thiagocarv4@gmail.com>
2026-03-20 18:50:15 -0300

    .

--- src/componets/mouseBounce.tsx
+++ src/componets/mouseBounce.tsx
@@ -3,7 +3,7 @@ interface MouseBounceProps {
 }
 export default function MouseBounce({ ...props }: MouseBounceProps) {
 	return (
-		<div className="absolute bottom-8 left-1/2 translate-x-1/2">
+		<div className="absolute bottom-8 left-1/2 translate-x-1/2" {...props}>
 			<div className="w-6 animate-bounce h-10 border-2 border-gray-300 rounded-full flex justify-center pt-2">
 				<div className="w-1 h-2 bg-yellow rounded-full"></div>
 			</div>
